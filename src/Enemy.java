a37ee23bf147de593f5b0f57db98b85f6f93bb5b
Aditya Davasam <aditya.davasam@gmail.com>
2025-11-07 09:24:31 -0800

    Initialized Enemy Class

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,2 +1,30 @@
 public class Enemy {
+    private float x,y,radius,speed;
+    public Enemy(float x, float y, float radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed = (radius/ (radius*1.44f)) * 10;
+    }
+    public boolean biggerThanPlayer(Player player) {
+        if (this.radius >= player.getRadius()) return true;
+        return false;
+    }
+    public void update(Player player) {
+        float deltaX = player.getX() - this.x;
+        float deltaY = player.getY() - this.y;
+        float distance = (float) Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+        float angle = (float) Math.atan2(deltaY,deltaX);
+        if (distance < 6700) {
+            if (this.biggerThanPlayer(player)) {
+                x += speed * Math.cos(angle);
+                y += speed * Math.sin(angle);
+            } else {
+                x -= speed * Math.cos(angle);
+                y -= speed * Math.sin(angle);
+            }
+        } else {
+            //random speed
+        }
+    }
 }
