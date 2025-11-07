ae682fff8227ff41d1a65db99cdd32ceed69c6af
homergubson <vihanparipatyadar@gmail.com>
2025-11-07 08:35:24 -0800

    Updated Food class

--- src/Food.java
+++ src/Food.java
@@ -1,15 +1,51 @@
+import processing.core.PApplet;
+
+import java.util.ArrayList;
+
 public class Food {
-    double x, y, radius;
-    public Food(double x, double y) {
+    float x, y, radius;
+    private int time;
+    private float speed;
+    private float acceleration;
+    public static ArrayList<Food> allfoods = new ArrayList<>();
+    public Food(float x, float y) {
         this.x = x;
         this.y = y;
         this.radius = 20;
+        this.time = 0;
+        this.speed = 0;
+        this.acceleration = 0;
+    }
+    public Food(float x, float y, float speed, float acceleration) {
+        this.x = x;
+        this.y = y;
+        this.radius = 20;
+        this.time = 0;
+        this.speed = speed;
+        this.acceleration = acceleration;
     }
     public void foodHitPlayer(Player player) {
-        double xDiff = Math.abs(this.x - player.getX());
-        double yDiff = Math.abs(this.y - player.getY());
-        double totalDistance = Math.sqrt((xDiff * xDiff) + (yDiff*yDiff));
-        double minimumDistance = this.radius + player.getRadius();
+        float xDiff = Math.abs(this.x - player.getX());
+        float yDiff = Math.abs(this.y - player.getY());
+        float totalDistance = (float) Math.sqrt((xDiff * xDiff) + (yDiff*yDiff));
+        float minimumDistance = this.radius + player.getRadius();
         if (totalDistance <= minimumDistance && player.getRadius() <= 60) player.setRadius(player.getRadius()+1);
+    }
+    public void draw(PApplet window) {
+        time++;
+        if(time>=60){
+            time=0;
+            allfoods.add(new Food((float) (Math.random()*10000),(float) (Math.random()*10000)));
+        }
+        window.fill(255,0,0);
+        for (int i = 0; i < allfoods.size(); i++) {
+            window.ellipse(this.x, this.y, this.radius*2, this.radius*2);
+        }
+
+    }
+    public void update(float angle) {
+        if(speed<0) speed-=acceleration;
+        x += speed * Math.cos(angle);
+        y += speed * Math.sin(angle);
     }
 }
